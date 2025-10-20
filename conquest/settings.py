06078ccf01df2eb84224b658647440704333183b
daequansession <ddae20208@gmail.com>
2025-10-20 14:45:10 -0400

    resolve settings file

--- conquest/settings.py
+++ conquest/settings.py
@@ -109,10 +109,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'conquest',
-        'USER': 'conquest_admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'USER': 'conquest_admin',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
