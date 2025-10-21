24bcf83b2e94078648e18aade8b9828b995c0109
jesse <jessebellsl2@gmail.com>
2025-10-21 11:56:26 -0400

    addassets

--- main_app/models.py
+++ main_app/models.py
@@ -9,22 +9,49 @@ CHARACTERS = (
     ('A', 'Holy Paladin'),
     ('B', 'Primal Barbarian'),
     ('C', 'Dragon Knight'),
+    ('D', 'Shadow Assassin'),
+    ('E', 'Demon Hunter'),
+    ('F', 'Chackie Jan'),
+    ('G', 'Hasidic Warrior'),
+    ('H', 'Mexican Vaquero'),
+    ('I', 'Death Knight'),
+    ('J', 'Every Italian Ever')
+
 )
 SHIELDS = (
     ('S1', 'Oceans Defender'),
     ('S2', 'Skyward Kite'),
-    ('S3', 'Earthguard Tower')
+    ('S3', 'Earthguard Tower'),
+    ('S4', 'Aether Guard Shield'),
+    ('S5', 'Correded Heater'),
+    ('S6', 'Ironbound Wooden Shield'),
+    ('S7', 'Leostrong Bastion'),
+    ('S8', 'Mechanum Defender'),
+    ('S9', 'Nightfeather Bulwark'),
+    ('S10', 'Plainsteel Buckler'),
+    ('S11', 'Serpentbloom Aegis'),
+    ('S12', 'Bloodborn Edgeguard')
+
 )
 WEAPONS = (
     ('W1', 'Natures Chopper'),
     ('W2', 'Fireborn Spear'),
-    ('W3', 'Frostblade Claymore')
+    ('W3', 'Frostblade Claymore'),
+    ('W4', 'Battleworn Hatchet'),
+    ('W5', 'Common Broadsword'),
+    ('W6', 'Forgebreak Polearm'),
+    ('W7', 'Ironclad Shortblade'),
+    ('W8', 'Nightslayers Scimitar'),
+    ('W9', 'Plaugeringers Scythe'),
+    ('W10', 'Shadowborn Blade'),
+    ('W11', 'Shadowfeather Dagger'),
+    ('W12', 'Solaris Glaive')
 )
 
 class Weapon(models.Model):
     name = models.CharField(max_length=50)
     weapon = models.CharField(
-        max_length=2,
+        max_length=3,
         choices=WEAPONS,
         default=WEAPONS[0][0]
     )
@@ -37,7 +64,7 @@ class Weapon(models.Model):
 class Shield(models.Model):
     name = models.CharField(max_length=50)
     shield = models.CharField(
-        max_length=2,
+        max_length=3,
         choices=SHIELDS,
         default=SHIELDS[0][0]
     )
